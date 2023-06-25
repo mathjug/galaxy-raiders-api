3c6fefed3e8b7012c3a773c42f3661d101362c77
BrunoArmondBraga <abraga3547bruno@gmail.com>
2023-06-25 10:04:39 -0300

    Implements Explosion class

---
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -0,0 +1,25 @@
+package galaxyraiders.core.game
+
+import galaxyraiders.core.physics.Point2D
+import galaxyraiders.core.physics.Vector2D
+
+class Explosion(
+  initialPosition: Point2D,
+  initialVelocity: Vector2D,
+  radius: Double,
+  mass: Double,
+) :
+  SpaceObject("Explosion", '*', initialPosition, initialVelocity, radius, mass){
+
+  var isTriggered: Boolean = false
+  
+  var tempo_de_vida: Int = 0
+    private set
+  
+  fun comeca(){
+    tempo_de_vida = tempo_de_vida + 1
+    if(tempo_de_vida > 10){
+      isTriggered = true
+    }
+  }
+  }
