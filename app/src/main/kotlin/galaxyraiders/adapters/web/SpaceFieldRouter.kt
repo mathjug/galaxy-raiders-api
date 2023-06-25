615eccd3937fd14ed3f64b3864721358d892c15b
BrunoArmondBraga <abraga3547bruno@gmail.com>
2023-06-25 10:23:53 -0300

    Add explosions to web

--- app/src/main/kotlin/galaxyraiders/adapters/web/SpaceFieldRouter.kt
+++ app/src/main/kotlin/galaxyraiders/adapters/web/SpaceFieldRouter.kt
@@ -8,12 +8,14 @@ import galaxyraiders.ports.ui.Visualizer
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.EndpointGroup
 import io.javalin.http.Context
+import galaxyraiders.core.game.Explosion
 
 class SpaceFieldRouter : Router, Visualizer {
   data class SpaceFieldDTO(
     val ship: SpaceShip,
     val asteroids: List<Asteroid>,
     val missiles: List<Missile>,
+    val explosions: List<Explosion>,
   )
 
   var dto: SpaceFieldDTO? = null
@@ -34,6 +36,7 @@ class SpaceFieldRouter : Router, Visualizer {
       ship = field.ship,
       asteroids = field.asteroids,
       missiles = field.missiles,
+      explosions = field.explosions,
     )
   }
 }
